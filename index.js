26379d92bcec366498f67f36b5cb051dfc1e06f7
garr3tt1989@gmail.com Hausman <garr3tt1989@gmail.com>
2018-08-13 18:13:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!"
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) 
-    return "I can\'t hear you!"
+  if (string.toUpperCase() === string) 
+    return "YES INDEED!"
+}
+
     
     
